694a5da53f27a92954bcbc9a3fb8f57f08252d02
AzulMachorro <72824518+AzulMachorro@users.noreply.github.com>
2022-11-09 18:21:25 -0600

    Update efecto_espejo.c

--- efecto_espejo.c
+++ efecto_espejo.c
@@ -10,9 +10,10 @@ int main()
     outputImage = fopen("img3.bmp","wb");    //Imagen transformada
     long ancho;
     long alto;
-    unsigned char r, g, b;               //Pixel
+    unsigned char r, g, b;               //Color del Pixel
     unsigned char* ptr;
 
+    //defini el 100 ya que asi se lograba una mejor nitidez en la imagen
     unsigned char xx[100];
     int cuenta = 0;
     for(int i=0; i<100; i++) {
@@ -42,17 +43,19 @@ int main()
       cuenta++;
 
     }                                        //Grises
+    //en ancho se multiplica por dos para obtener mejor resultado en el acomodo de los pixeles
     cuenta = ancho*2;
     #pragma omp parallel
     {
       #pragma omp for schedule (dynamic)
     for (int i = 0; i < alto*ancho*3; ++i) {
+        // en esta parte tambien modifique algunas operaciones para que los pixeles siguieran un orden al acomodarce y no se generaran desplazamientos
       fputc(ptr[i+(cuenta+cuenta+2)], outputImage);
       fputc(ptr[i+(cuenta+cuenta+1)+1], outputImage);
       fputc(ptr[i+(cuenta+cuenta)+2], outputImage);
       cuenta--;
       if (cuenta == 0){
-        cuenta = (ancho*2);
+        cuenta = ancho*2;
       }
     }
     }
